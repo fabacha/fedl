cda580ce0d4f46648b1c078f3751f7d27d5f0a67
Fatima <76473050+fabacha@users.noreply.github.com>
2023-09-26 19:59:15 +0100

    Update test.py

--- models/test.py
+++ models/test.py
@@ -6,7 +6,8 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-
+import wandb
+wandb.init(entity= 'fabacha22', project='Fed_L_Review')
 
 def test_img(net_g, datatest, args):
     net_g.eval()
@@ -21,14 +22,18 @@ def test_img(net_g, datatest, args):
         log_probs = net_g(data)
         # sum up batch loss
         test_loss += F.cross_entropy(log_probs, target, reduction='sum').item()
+
         # get the index of the max log-probability
         y_pred = log_probs.data.max(1, keepdim=True)[1]
         correct += y_pred.eq(target.data.view_as(y_pred)).long().cpu().sum()
+    wandb.log({'Correct': correct})
 
     test_loss /= len(data_loader.dataset)
     accuracy = 100.00 * correct / len(data_loader.dataset)
+    wandb.log({'Test Loss': test_loss})
+    wandb.log({'Accuracy': accuracy})
     if args.verbose:
         print('\nTest set: Average loss: {:.4f} \nAccuracy: {}/{} ({:.2f}%)\n'.format(
             test_loss, correct, len(data_loader.dataset), accuracy))
-    return accuracy, test_loss
 
+    return accuracy, test_loss
