7ecbe9c2e806204366253f6f8d9abdd5a79a46db
Fatima <76473050+fabacha@users.noreply.github.com>
2023-09-30 08:03:33 +0100

    Create sampling_review.py

---
+++ utils/sampling_review.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+
+import numpy as np
+from torchvision import datasets, transforms
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import copy
+import numpy as np
+import torch
+from torch.utils.data import Subset
+
+def mnist_iid(dataset, num_users):
+    """
+    Sample I.I.D. client data from MNIST dataset
+    :param dataset:
+    :param num_users:
+    :return: dict of image index
+    """
+    num_items = int(len(dataset)/num_users)
+    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    for i in range(num_users):
+        dict_users[i] = set(np.random.choice(all_idxs, num_items, replace=False))
+        all_idxs = list(set(all_idxs) - dict_users[i])
+    return dict_users
+
+
+trans_mnist = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+mnist_train = datasets.MNIST('../data/mnist/', train=True, download=True, transform=trans_mnist)
+subset_size = 60000
+subset_indices = torch.randperm(len(mnist_train))[:subset_size]
+dataset_train = Subset(mnist_train, subset_indices)
+
+def mnist_noniid(dataset_train, num_users):
+    """
+    Sample non-I.I.D client data from MNIST dataset
+    :param dataset:
+    :param num_users:
+    :return:
+    """
+
+    #dataset_train.train_labels = torch.tensor([dataset_train.dataset.targets[i] for i in subset_indices])
+    #dataset_test = datasets.MNIST('../data/mnist/', train=False, download=True, transform=trans_mnist)
+    # sample users
+    num_shards, num_imgs = 300, 200
+    idx_shard = [i for i in range(num_shards)]
+    dict_users = {i: np.array([], dtype='int64') for i in range(num_users)}
+    idxs = np.arange(num_shards*num_imgs)
+    # subset_size = 30000
+    # subset_indices = torch.randperm((60000))[:subset_size]
+    dataset_train.train_labels = torch.tensor([dataset_train.dataset.targets[i] for i in subset_indices])
+    labels = dataset_train.train_labels.numpy()
+    #labels=torch.tensor([dataset_train.dataset.targets[i] for i in subset_indices])
+
+    # sort labels
+    idxs_labels = np.vstack((idxs, labels))
+    idxs_labels = idxs_labels[:,idxs_labels[1,:].argsort()]
+    idxs = idxs_labels[0,:]
+
+    # divide and assign
+    for i in range(num_users):
+        rand_set = set(np.random.choice(idx_shard, 2, replace=False))
+        idx_shard = list(set(idx_shard) - rand_set)
+        for rand in rand_set:
+            dict_users[i] = np.concatenate((dict_users[i], idxs[rand*num_imgs:(rand+1)*num_imgs]), axis=0)
+    return dict_users
+
+
+def cifar_iid(dataset, num_users):
+    """
+    Sample I.I.D. client data from CIFAR10 dataset
+    :param dataset:
+    :param num_users:
+    :return: dict of image index
+    """
+    num_items = int(len(dataset)/num_users)
+    dict_users, all_idxs = {}, [i for i in range(len(dataset))]
+    for i in range(num_users):
+        dict_users[i] = set(np.random.choice(all_idxs, num_items, replace=False))
+        all_idxs = list(set(all_idxs) - dict_users[i])
+    return dict_users
+
+
+if __name__ == '__main__':
+    dataset_train = datasets.MNIST('../data/mnist/', train=True, download=True,
+                                   transform=transforms.Compose([
+                                       transforms.ToTensor(),
+                                       transforms.Normalize((0.1307,), (0.3081,))
+                                   ]))
+    num = 100
+    d = mnist_noniid(dataset_train, num)
